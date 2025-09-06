Refactoring the concept_loss out of the loss computation to isolate the bug where it turns negative around epoch 90

models/losses.py
@@ -158,20 +158,7 @@ class SCBLoss(nn.Module):
         Returns:
             Tensor: Target loss, concept loss, precision loss, and total loss.
         """
-
-        assert torch.all((concepts_true == 0) | (concepts_true == 1))
-        concepts_true_expanded = concepts_true.unsqueeze(-1).expand_as(
-            concepts_mcmc_probs
-        )
-
-        bce_loss = F.binary_cross_entropy(
-            concepts_mcmc_probs, concepts_true_expanded.float(), reduction="none"
-        )  # [B,C,MCMC]
-        intermediate_concepts_loss = -torch.sum(bce_loss, dim=1)  # [B,MCMC]
-        mcmc_loss = -torch.logsumexp(
-            intermediate_concepts_loss, dim=1
-        )  # [B], logsumexp for numerical stability due to shift invariance
-        concepts_loss = self.alpha * torch.mean(mcmc_loss)
+        concepts_loss = self.compute_concept_loss(concepts_mcmc_probs, concepts_true)
 
         if self.num_classes == 2:
             # Logits to probs
@@ -209,3 +196,18 @@ class SCBLoss(nn.Module):
         total_loss = target_loss + concepts_loss + prec_loss
 
         return target_loss, concepts_loss, prec_loss, total_loss
+
+    def compute_concept_loss(self, concepts_mcmc_probs, concepts_true):
+        assert torch.all((concepts_true == 0) | (concepts_true == 1))
+        concepts_true_expanded = concepts_true.unsqueeze(-1).expand_as(
+            concepts_mcmc_probs
+        )
+
+        bce_loss = F.binary_cross_entropy(
+            concepts_mcmc_probs, concepts_true_expanded.float(), reduction="none"
+        )  # [B,C,MCMC]
+        intermediate_concepts_loss = -torch.sum(bce_loss, dim=1)  # [B,MCMC]
+        mcmc_loss = -torch.logsumexp(
+            intermediate_concepts_loss, dim=1
+        )  # [B], logsumexp for numerical stability due to shift invariance
+        return self.alpha * torch.mean(mcmc_loss)